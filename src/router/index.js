132b6f6727eb34402be048311e9c4147237fdc3d
julienturck <julien.turck.tic@gmail.com>
2024-06-03 11:10:03 +0200

    corrections

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,14 @@ import NotFound from '../views/NotFound.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    
     {
-      path: '/',
+      path: '/Accueil.vue',
       name: 'Accueil',
       component: Accueil
     },
     {
-      path: '/Accueil.vue',
+      path: '/',
       name: 'Accueil',
       component: Accueil
     },
